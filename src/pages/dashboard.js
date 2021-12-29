f3fbb908b660c3f5558f16ee0fe2a39b39098772
Abbas roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2021-12-29 14:05:16 +0430

    create layout

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,20 +1,7 @@
 import { React } from "react";
-import { Row, Col } from "react-bootstrap";
 
 const Dashboard = () => {
-  return (
-    <div className="container-fluid">
-      <Row>
-        <Col lg="1">sidebar</Col>
-        <Col lg="11">
-          <Row>header</Row>
-          <Row>after header</Row>
-          <Row>zia</Row>
-          <Row>abbase</Row>
-        </Col>
-      </Row>
-    </div>
-  );
+  return <h3>dashboard</h3>;
 };
 
 export default Dashboard;
