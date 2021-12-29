41ef3f66dca58770809d05c4e8a1cd45fe7bddc9
sayedhassan-husssaini <sh.hussaini1378@gmail.com>
2021-12-29 11:55:33 +0430

    create common filder

--- src/components/common/Sidebar.js
+++ src/components/common/Sidebar.js
@@ -1,11 +1,13 @@
 import { React } from "react";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col,Image } from "react-bootstrap";
 
 const Sidebar = () => {
   return (
     <div className="container-fluid">
       <Row className="pt-4">
-        <Col>Icon 1</Col>
+        <Col>
+           <Image src="/icone/countdown to break 3.png"/>
+        </Col>
         <Col>Icon 2</Col>
       </Row>
     </div>
