4b2472628efc8e25fe022cbd00c490f4ebe56d03
sayedhassan-husssaini <sh.hussaini1378@gmail.com>
2021-12-29 12:09:26 +0430

    set icone in sidebar

--- src/components/common/Sidebar.js
+++ src/components/common/Sidebar.js
@@ -1,14 +1,50 @@
 import { React } from "react";
 import { Row, Col,Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <div className="container-fluid">
       <Row className="pt-4">
         <Col>
-           <Image src="/icone/countdown to break 3.png"/>
+           <Link to=""><Image src="/icone/countdown to break 3.png"/></Link>
         </Col>
-        <Col>Icon 2</Col>
+        <Col>
+           <Link to=""><Image src="/icone/countdown to break 1.png"/></Link>
+        </Col>
+        <Col>
+           <Link to=""><Image src="/icone/kittysplit 1.png"/></Link>
+        </Col>
+        <Col>
+           <Link to=""><Image src="/icone/task manager 1.png"/></Link>
+        </Col>
+        <Col>
+           <Link to=""><Image src="/icone/Vector.png"/></Link>
+        </Col>
+        <Col>
+           <Link to=""><Image src="/icone/eye 1.png"/></Link>
+        </Col>
+        <Col>
+           <Link to=""><Image src="/icone/task manager 1.png"/></Link>
+        </Col>
+        <Col>
+           <Link to=""><Image src="/icone/exclamation-mark 7.png"/></Link>
+        </Col>
+        <Col>
+           <Link to=""><Image src="/icone/exclamation-mark 3.png"/></Link>
+        </Col>
+        <Col>
+           <Link to=""><Image src="/icone/exclamation-mark 4.png"/></Link>
+        </Col>
+        <Col>
+           <Link to=""><Image src="/icone/exclamation-mark 5.png"/></Link>
+        </Col>
+        <Col>
+           <Link to=""><Image src="/icone/exclamation-mark 6.png"/></Link>
+        </Col>
+        <Col>
+           <Link to=""><Image src="/icone/blue-book 1.png"/></Link>
+        </Col>
       </Row>
     </div>
   );
