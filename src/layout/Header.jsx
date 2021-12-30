811a31a78d78a213efac3177fcd7218eeda5cea7
sayedhassan-husssaini <sh.hussaini1378@gmail.com>
2021-12-30 14:22:26 +0430

    fix style of header

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,20 +1,34 @@
 import { React } from "react";
-import {Row,Col,Form } from "react-bootstrap";
+import { Row, Col, Form,Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <>
       <Col className="col-12 header-name">Hi Denail</Col>
-      <Row>
-      <Col className="text-secondary-dark header-thank mt-2" >Thank god it’s friday!</Col>
-      <Col>
-        <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control type="email" placeholder="Enter email" />
-          </Form.Group>
-        </Form>
-      </Col>
+      <Row className="mb-3">
+        <Col className="col-9 text-secondary-dark header-thank mt-3" >Thank god it’s friday!</Col>
+        <Col className="col-3">
+          <Row>
+            <Col className="col-8">
+              <Form className="form-search">
+                <Form.Group className="mb-3 serach-input" controlId="formBasicEmail">
+                  <Form.Control type="search" placeholder="search" />
+                </Form.Group>
+              </Form>
+            </Col>
+            <Col className="col-2">
+              <div className="header-icon navy-blue text-center pt-2">
+              <Link to=""><Image className="sidebar-icon" src="/icone/hcphotos-Headshots-1 1.png" /></Link>
+              </div>
+            </Col>
+            <Col className="col-2">
+              <div className="header-icon navy-blue text-center pt-2">
+              <Link to=""><Image className="sidebar-icon" src="/icone/hcphotos-Headshots-1 2.png" /></Link>
+              </div>
+            </Col>
+          </Row>
+        </Col>
       </Row>
     </>
   );
