aec63a370416df5f8e2925fe0a2193caabd7ac45
sayedhassan-husssaini <sh.hussaini1378@gmail.com>
2021-12-30 13:20:04 +0430

    change dashboard page

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,7 +1,54 @@
 import { React } from "react";
-
+import { Row, Col, Image } from "react-bootstrap";
+import CardHeader from "../components/card/CardHeader";
+import WaterPool from "../components/WaterPool";
+import Card from "./../components/card/Card";
 const Dashboard = () => {
-  return <h3>dashboard</h3>;
+  return (
+    <section>
+      <Row>
+        <Col xl={4}>
+          <Card>
+            <CardHeader
+              icon={<Image src="/icone/Vector.png" alt="vector image" />}
+              title={
+                <h4 className="heading4 secondar-color ">Hydration Reminder</h4>
+              }
+              action={
+                <>
+                  <i className="fa fa-plus" aria-hidden="true"></i>
+                  <span>i</span>
+                </>
+              }
+            />
+            <div className="pt-4">
+              <WaterPool />
+            </div>
+          </Card>
+        </Col>
+        <Col xl={4}>
+          <Card>
+            <CardHeader>
+              <h1>hello</h1>
+            </CardHeader>
+          </Card>
+          <Card>
+            <CardHeader>
+              <h1>hello</h1>
+            </CardHeader>
+          </Card>
+        </Col>
+        <Col xl={4}>
+          <Card>
+            <CardHeader>
+              <h1>hello</h1>
+            </CardHeader>
+          </Card>
+        </Col>
+      </Row>
+    </section>
+  );
 };
 
 export default Dashboard;
+
