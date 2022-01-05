22e938e117678b00353998b2df7a4b8fe728eb9b
sayedhassan-husssaini <sh.hussaini1378@gmail.com>
2022-01-05 14:55:15 +0430

    change register form

--- src/components/user/register/Company.jsx
+++ src/components/user/register/Company.jsx
@@ -4,7 +4,7 @@ import { Icon } from "@iconify/react";
 import { Link } from "react-router-dom";
 import style from '../style.module.css'
 const CompanyRegister = () => {
-  
+
   const [sendEmail, setSendEmail] = useState(false)
   const handleRegister = () => {
     setSendEmail(true)
@@ -13,8 +13,8 @@ const CompanyRegister = () => {
     <div className={style.registerPage}>
       {
         !sendEmail
-          ? <Row className="p-0 m-0 row justify-content-center">
-            <Col xl='6'>
+          ? <Row className="p-0 m-0 row">
+            <Col xl='8'>
               <div className={style.registerCard}>
                 <div className={`${style.header}  text-center pt-4`}>
                   <div className={style.floatLeft}>1/2</div>
@@ -26,31 +26,27 @@ const CompanyRegister = () => {
                 <div className={style.body}>
                   <Form>
                     <Row>
-                      <Col xl='6'>
-                        <Row>
-                          <Col xl='6'>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                              <Form.Label className={style.lableForm}>First Name *</Form.Label>
-                              <Form.Control
-                                className={style.formInput}
-                                type="text"
-                                placeholder="First Name"
-                              />
-                            </Form.Group>
-                          </Col>
-                          <Col xl='6'>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                              <Form.Label className={style.lableForm}>last Name *</Form.Label>
-                              <Form.Control
-                                className={style.formInput}
-                                type="text"
-                                placeholder="Last name"
-                              />
-                            </Form.Group>
-                          </Col>
-                        </Row>
+                      <Col xl="3">
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                          <Form.Label className={style.lableForm}>First Name *</Form.Label>
+                          <Form.Control
+                            className={style.formInput}
+                            type="text"
+                            placeholder="First Name"
+                          />
+                        </Form.Group>
                       </Col>
-                      <Col xl='6'>
+                      <Col xl="3">
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                          <Form.Label className={style.lableForm}>last Name *</Form.Label>
+                          <Form.Control
+                            className={style.formInput}
+                            type="text"
+                            placeholder="Last name"
+                          />
+                        </Form.Group>
+                      </Col>
+                      <Col xl="6">
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                           <Form.Label className={style.lableForm}>E-mail *</Form.Label>
                           <Form.Control
@@ -60,9 +56,7 @@ const CompanyRegister = () => {
                           />
                         </Form.Group>
                       </Col>
-                    </Row>
-                    <Row>
-                      <Col xl='4'>
+                      <Col xl="6">
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                           <Form.Label className={style.lableForm}>Company Name *</Form.Label>
                           <Form.Control
@@ -71,9 +65,8 @@ const CompanyRegister = () => {
                             placeholder="Company Name"
                           />
                         </Form.Group>
-
                       </Col>
-                      <Col xl='4'>
+                      <Col xl='6'>
                         <Form.Group className="mb-3">
                           <Form.Label className={style.lableForm}>Company Size *</Form.Label>
                           <Form.Control
@@ -83,18 +76,16 @@ const CompanyRegister = () => {
                           />
                         </Form.Group>
                       </Col>
-                      <Col xl='4'>
+                      <Col xl='6'>
                         <Form.Group className="mb-3">
-                          <Form.Label className={style.lableForm}>Text ID *</Form.Label>
+                          <Form.Label className={style.lableForm}>Tex ID *</Form.Label>
                           <Form.Control
                             className={style.formInput}
                             type="number"
-                            placeholder="Text ID"
+                            placeholder="Tex ID"
                           />
                         </Form.Group>
                       </Col>
-                    </Row>
-                    <Row>
                       <Col xl='6'>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                           <Form.Label className={style.lableForm}>Web Site *</Form.Label>
@@ -115,9 +106,7 @@ const CompanyRegister = () => {
                           />
                         </Form.Group>
                       </Col>
-                    </Row>
-                    <Row>
-                      <Col xl='4'>
+                      <Col xl='6'>
                         <Form.Group className="mb-3">
                           <Form.Label className={style.lableForm}>Country *</Form.Label>
                           <Form.Select className={style.formInput} aria-label="Default select example">
@@ -128,7 +117,7 @@ const CompanyRegister = () => {
                           </Form.Select>
                         </Form.Group>
                       </Col>
-                      <Col xl='4'>
+                      <Col xl='6'>
                         <Form.Group className="mb-3">
                           <Form.Label className={style.lableForm}>City *</Form.Label>
                           <Form.Select className={style.formInput} aria-label="Default select example">
@@ -139,7 +128,7 @@ const CompanyRegister = () => {
                           </Form.Select>
                         </Form.Group>
                       </Col>
-                      <Col xl='4'>
+                      <Col xl='6'>
                         <Form.Group className="mb-3">
                           <Form.Label className={style.lableForm}>State *</Form.Label>
                           <Form.Select className={style.formInput} aria-label="Default select example">
@@ -150,10 +139,8 @@ const CompanyRegister = () => {
                           </Form.Select>
                         </Form.Group>
                       </Col>
-                    </Row>
-                    <Row>
                       <Col xl='6'>
-                      <Form.Group className="mb-3">
+                        <Form.Group className="mb-3">
                           <Form.Label className={style.lableForm}>Street *</Form.Label>
                           <Form.Control
                             className={style.formInput}
@@ -163,7 +150,7 @@ const CompanyRegister = () => {
                         </Form.Group>
                       </Col>
                       <Col xl='6'>
-                      <Form.Group className="mb-3">
+                        <Form.Group className="mb-3">
                           <Form.Label className={style.lableForm}>Postal code *</Form.Label>
                           <Form.Control
                             className={style.formInput}
@@ -172,41 +159,18 @@ const CompanyRegister = () => {
                           />
                         </Form.Group>
                       </Col>
+                      <Row className="justify-content-center">
+                        <Col xl='6'>
+                          <Button
+                            className={style.submitBtn}
+                            type="button"
+                            onClick={(e) => { handleRegister(e) }}
+                          >
+                            REGISTER
+                          </Button>
+                        </Col>
+                      </Row>
                     </Row>
-
-                    {/* <Form.Group className="mb-3" controlId="formBasicPassword">
-                  <Form.Label className={style.lableForm}>Password</Form.Label>
-                  <div className="mb-3 input-group">
-                    <Form.Control
-                      className={style.formInput}
-                      type={`${showPassword ? "text" : "password"}`}
-                      placeholder="Password"
-                    />
-                    <i
-                      onClick={() => setShowPassword(!showPassword)}
-                      className={`${style.formInput} ${style.passwordIcon} input-group-text`}
-                      id="btnGroupAddon"
-                    >
-                      {!showPassword ? (
-                        <Icon icon="akar-icons:eye" />
-                      ) : (
-                        <Icon icon="clarity:eye-hide-line" />
-                      )}
-                    </i>
-                  </div>
-                </Form.Group> */}
-
-                    <Button
-                      className={style.submitBtn}
-                      type="button"
-                      onClick={(e) => { handleRegister(e) }}
-                    >
-                      REGISTER
-                    </Button>
-                    {/* <button className={`${style.btnGoogle} mt-4`} type="submit">
-                  <Icon className="google-icon" icon="flat-color-icons:google" />
-                  Register with Google
-                </button> */}
                   </Form>
                 </div>
               </div>
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,11 +1,22 @@
 import { React } from "react";
 import { Outlet } from "react-router-dom";
+import { Container, Row, Col,Image } from "react-bootstrap";
 
 const Register = () => {
   return (
-   <>
-      <Outlet />
-   </>
+    <>
+      {/* <Outlet /> */}
+      <Container className="secondary-color" fluid>
+        <Row>
+          <Col xl='5' className="pt-3 text-center">
+            <Image src='/img/register.png' />
+          </Col>
+          <Col xl='7'>
+            <Outlet />
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
