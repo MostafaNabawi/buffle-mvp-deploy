e25cd54e6c806628967cc0c377ec4399db8496ea
sayedhassan-husssaini <sh.hussaini1378@gmail.com>
2022-01-08 09:39:38 +0430

    fix breack plan title

--- src/components/breakplan/BreakplanForm.jsx
+++ src/components/breakplan/BreakplanForm.jsx
@@ -4,7 +4,7 @@ import { Icon } from '@iconify/react';
 import style from './style.module.css'
 
 function BreackplanFrom({
-    title, show, setShow, newTime, joinOrSagest
+    show, setShow, newTime, joinOrSagest
 }) {
     const [close, setClose] = useState(true)
     const [newSaggestion, setNewSaggestion] = useState(false)
@@ -16,49 +16,55 @@ function BreackplanFrom({
     return (
         <div className={`${style.manCard} ${close ? style.hide : style.show}`}>
             <Card className={`${style.customCard} pb-1`}>
-               <div>
-               <i className={style.closeIcon} onClick={() => {
-                    setShow(false)
-                    setClose(true)
-                }} ><Icon icon="ci:close-big" /></i>
-               </div>
+                <div>
+                    <i className={style.closeIcon} onClick={() => {
+                        setShow(false)
+                        setClose(true)
+                    }} ><Icon icon="ci:close-big" /></i>
+                </div>
                 <Card.Body>
-                    <Card.Title>
-                        {title}
-                    </Card.Title>
                     {
                         joinOrSagest
-                            ? <Card.Text className="text-center">
-                                <Button variant="outline-primary" onClick={() => { setNewSaggestion(false) }} className={style.customBtn}>Join</Button>
-                                <Button variant="outline-primary" onClick={() => { setNewSaggestion(!newSaggestion) }} className={style.customBtn}>Suggestion</Button>
-                                {
-                                    newSaggestion
-                                        ?
-                                        <Form className="mt-3">
-                                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                                <Form.Control
-                                                    autoFocus
-                                                    as="textarea"
-                                                    type="email"
-                                                    placeholder="New Saggestion"
-                                                />
-                                            </Form.Group>
-                                            <Button className={style.withBtn} variant="primary" type="submit">
-                                                Send
-                                            </Button>
-                                        </Form>
-                                        : ""
-                                }
-                            </Card.Text>
+                            ?
+                            <>
+                                {/* <Card.Title className={style.tilte}>
+                                    Join Or new Sagest
+                                </Card.Title> */}
+                                <Card.Text className="text-center">
+                                    <Button variant="outline-primary" onClick={() => { setNewSaggestion(false) }} className={style.customBtn}>Join</Button>
+                                    <Button variant="outline-primary" onClick={() => { setNewSaggestion(!newSaggestion) }} className={style.customBtn}>Suggestion</Button>
+                                    {
+                                        newSaggestion
+                                            ?
+                                            <Form className="mt-3">
+                                                <Form.Group className="mb-3" controlId="formBasicEmail">
+                                                    <Form.Control
+                                                        autoFocus
+                                                        as="textarea"
+                                                        type="email"
+                                                        placeholder="New Saggestion"
+                                                    />
+                                                </Form.Group>
+                                                <Button className={style.withBtn} variant="primary" type="submit">
+                                                    Send
+                                                </Button>
+                                            </Form>
+                                            : ""
+                                    }
+                                </Card.Text>
+                            </>
                             :
                             newTime ?
+                             <>
+                             <Card.Title className={style.tilte}>
+                             Saggest new time
+                                </Card.Title> 
                                 <Card.Text className="text-center">
-
                                     <Form className="mt-3">
                                         <Form.Group className="mb-3" controlId="formBasicEmail">
                                             <Form.Control
                                                 autoFocus
-                                                type="text"
+                                                type="time"
                                                 placeholder="Saggest new time"
                                             />
                                         </Form.Group>
@@ -67,25 +73,30 @@ function BreackplanFrom({
                                         </Button>
                                     </Form>
                                 </Card.Text>
-                                : 
+                             </>
+                                :<>
+                                <Card.Title className={style.tilte}>
+                               New breack pland
+                                   </Card.Title> 
                                 <Form className="mt-3">
-                                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                                            <Form.Control
-                                                autoFocus
-                                                type="text"
-                                                placeholder="Plan Title"
-                                            />
-                                        </Form.Group>
-                                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                                            <Form.Control
-                                                type="time"
-                                                placeholder="Time"
-                                            />
-                                        </Form.Group>
-                                        <Button className={style.withBtn} variant="primary" type="submit">
-                                          Set
-                                        </Button>
-                                    </Form>
+                                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                                        <Form.Control
+                                            autoFocus
+                                            type="text"
+                                            placeholder="Plan Title"
+                                        />
+                                    </Form.Group>
+                                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                                        <Form.Control
+                                            type="time"
+                                            placeholder="Time"
+                                        />
+                                    </Form.Group>
+                                    <Button className={style.withBtn} variant="primary" type="submit">
+                                        Set
+                                    </Button>
+                                </Form>
+                                </>
                     }
 
                 </Card.Body>
