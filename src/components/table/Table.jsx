9c2b8432687a205077f189e9529119cc331dc2c8
sayedhassan-husssaini <sh.hussaini1378@gmail.com>
2022-01-10 12:10:23 +0430

    set table for user list

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,16 +1,52 @@
 import { React } from "react";
-import { Table, Card } from "react-bootstrap";
+import { Table, Card, Pagination } from "react-bootstrap";
 import { Icon } from '@iconify/react';
+import style from './style.module.css'
 
-const TableList = ({ tableHeader, tableBody }) => {
+const TableList = ({
+    tableHeader, tableBody, tableAction,
+    headClass, bodyClass, actionClass,
+    isPagination
+}) => {
     return (
         <>
             <Card>
                 <Table responsive>
-                    {tableHeader}
-                    {tableBody}
-                   
+                    <thead className={headClass}>
+                        <tr>
+                            {tableHeader && (
+                                tableHeader.map(item => (
+                                    <th className={actionClass}>{item}</th>
+                                ))
+                            )}
+                            {tableAction && (
+                                <th>Action</th>
+                            )}
+                        </tr>
+                    </thead>
+                    <tbody className={bodyClass}>
+                        <tr>
+                            {tableBody && (
+                                tableBody.map(item => (
+                                    <td>{item}</td>
+                                ))
+                            )}
+                            {tableAction && (
+                                <td className={actionClass}>{tableAction}</td>
+                            )}
+                        </tr>
+                    </tbody>
                 </Table>
+                {
+                    isPagination && (
+                        <Pagination className={style.custompagination} >
+                            <Pagination.Prev />
+                            <Pagination.Item>{1}</Pagination.Item>
+                            <Pagination.Item>{2}</Pagination.Item>
+                            <Pagination.Next />
+                        </Pagination>
+                    )
+                }
             </Card>
         </>
     );
--- src/components/user/userList/UserList.jsx
+++ src/components/user/userList/UserList.jsx
@@ -1,5 +1,5 @@
 import { React } from "react";
-import { Container, Row} from "react-bootstrap";
+import { Button, Container, Row,Col} from "react-bootstrap";
 import { Icon } from '@iconify/react';
 import Table from "../../table/Table";
 
@@ -8,23 +8,14 @@ const UserList = () => {
         <>
             <Container className={`secondary-color pt-5 `} fluid>
                 <Row className="justify-content-center">
+                    <Col xl={9}>
                     <Table
-                     tableHeader={
-                        <thead>
-                        <tr>
-                            <th>#</th>
-                            
-                        </tr>
-                    </thead>
-                     }
-                     tableBody={
-                        <tbody>
-                        <tr>
-                            <td>1</td>
-                        </tr>
-                    </tbody>
-                     }
+                     tableHeader={["First Name","Last Name","Email","Cuntry","Type"]}
+                     tableBody={["Sayed Hassan","Hussaini","sh@gmail.com","Afg","Freelancer"]}
+                     tableAction=""
+                     isPagination={true}
                     />
+                    </Col>
                 </Row>
             </Container>
         </>
