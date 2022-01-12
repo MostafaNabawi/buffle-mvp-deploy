1a76551e8d7c85b11deba7cd6c90d810ef0156ac
Abbas roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2022-01-12 14:43:36 +0430

    fix repeat aption in modal

--- src/components/taskMnagement/project/CreateProject.jsx
+++ src/components/taskMnagement/project/CreateProject.jsx
@@ -1,47 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Form, Row, Col, Button } from "react-bootstrap";
 import { Icon } from "@iconify/react";
 import Modal from "../../modal/modal";
 
-
 const CreateProject = () => {
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    return (
-        <Row className="creat-project-row">
-            <Col lg="6">projects</Col>
-            <Col lg="6" className="creat-project-col">
-                <div className="creat-project-div">
-                    <span className="creat-project-plus"><Icon icon="bi:plus-lg" /></span>
-                    <span className="creat-project-btn" onClick={handleShow}>create project</span>
-                </div>
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <Row className="creat-project-row">
+      <Col lg="6">projects</Col>
+      <Col lg="6" className="creat-project-col">
+        <div className="creat-project-div">
+          <span className="creat-project-plus">
+            <Icon icon="bi:plus-lg" />
+          </span>
+          <span className="creat-project-btn" onClick={handleShow}>
+            create project
+          </span>
+        </div>
+      </Col>
+      <Modal
+        show={show}
+        handleClose={handleClose}
+        title="Create Project"
+        className="create-project-modal"
+        body={
+          <Row>
+            <Col md={12}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Control type="text" placeholder="Name your project..." />
+              </Form.Group>
             </Col>
-            <Modal
-                show={show}
-                handleClose={handleClose}
-                title="Create Project"
-                className="create-project-modal"
-                body={
-                    <Row>
-                        <Col md={12}>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Control type="text" placeholder='Name your project...' />
-                            </Form.Group>
-                        </Col>
-                    </Row>
-                }
-                footer={
-                    <>
-                        <Button onClick={handleClose}>Close</Button>
-                        <Button variant="primary" type="submit">
-                            Save
-                        </Button>
-
-                    </>
-                }
-            />
-        </Row>
-    )
-}
+          </Row>
+        }
+        footer={
+          <>
+            <Button onClick={handleClose}>Close</Button>
+            <Button variant="primary" type="submit">
+              Save
+            </Button>
+          </>
+        }
+      />
+    </Row>
+  );
+};
 export default CreateProject;
