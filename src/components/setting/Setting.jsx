dfd702c27318e64ce3cca3dc3df71b300b54e729
sayedhassan-husssaini <sh.hussaini1378@gmail.com>
2022-01-27 11:44:08 +0430

    fix invitation link

--- src/components/setting/Setting.jsx
+++ src/components/setting/Setting.jsx
@@ -1,54 +1,58 @@
-import { React,useState } from "react";
+import { React, useState } from "react";
 import { Container, Row, Col, Card, ListGroup, Button, Form } from "react-bootstrap";
 import { Icon } from '@iconify/react';
 import style from "./style.module.css"
-import { API_URL} from "../../config/index";
+import { API_URL } from "../../config/index";
 import { checkEmail } from "../../config/utils";
 import PulseLoader from "react-spinners/PulseLoader";
 import { useToasts } from "react-toast-notifications";
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 const Setting = () => {
     const { addToast } = useToasts();
-    const [email,setEmail]=useState('')
-    const [loading,setLoading]=useState(false)
+    const [email, setEmail] = useState('')
+    const [loading, setLoading] = useState(false)
+    const [loadingRege, setLoadingReg] = useState(false)
+    const [copyValue, setCopyValue] = useState('')
+    const [copied, setCopied] = useState(false)
 
-    const handleSubmit =async(e)=>{
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        if (email  === "") {
+        if (email === "") {
             addToast("Email is required!  👀", {
-              appearance: "warning",
-              autoDismiss: 4000,
+                appearance: "warning",
+                autoDismiss: 4000,
             });
             return;
-          }
+        }
         if (!checkEmail(email)) {
             addToast("Invalid Email!", {
-              appearance: "warning",
-              autoDismiss: 4000,
+                appearance: "warning",
+                autoDismiss: 4000,
             });
             return;
-          }
-          try{
-              setLoading(true)
-            await fetch(`${API_URL}/invite/invite-by-email`,{
+        }
+        try {
+            setLoading(true)
+            await fetch(`${API_URL}/invite/invite-by-email`, {
                 method: "POST",
                 credentials: "include",
                 headers: {
-                  "Content-Type": "application/json",
-                  "Access-Control-Allow-Credentials": true,
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Credentials": true,
                 },
                 body: JSON.stringify({
-                  email: email,
+                    email: email,
                 }),
-            }).then((res)=>{
-                if(res.status === 200){
+            }).then((res) => {
+                if (res.status === 200) {
                     setLoading(false)
                     addToast("Invitation email send!", {
                         appearance: "success",
                         autoDismiss: 4000,
                     });
                     setEmail('')
-                }else{
+                } else {
                     addToast("This email was not register in buffle. please tru again!", {
                         appearance: "error",
                         autoDismiss: 4000,
@@ -56,9 +60,37 @@ const Setting = () => {
                     setLoading(false)
                 }
             })
-          }catch{
-              setLoading(false)
-          }
+        } catch {
+            setLoading(false)
+        }
+    }
+    const handleRegeneration = async () => {
+        setCopyValue('')
+        try {
+            setLoadingReg(true)
+            await fetch(`${API_URL}/user/my-invite`, {
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Credentials": true,
+                },
+            }).then(async (res) => {
+                if (res.status === 200) {
+                    const { link } = await res.json()
+                    setLoadingReg(false)
+                    setCopyValue(link)
+                } else {
+                    addToast("Error please try Again!", {
+                        appearance: "error",
+                        autoDismiss: 4000,
+                    });
+                    setLoadingReg(false)
+                }
+            })
+        } catch {
+            console.log("server error")
+            setLoadingReg(false)
+        }
     }
     return (
         <Col xl="8" className="pt-5">
@@ -66,11 +98,22 @@ const Setting = () => {
                 <ListGroup variant="flush">
                     <ListGroup.Item className="pb-3">
                         <h4>Invite Link</h4>
-                        <i>https://join.weekrise.com/Fjabaakoiuhliuhsp51C72m8</i>
+                        {copyValue
+                            ?
+                            <i>{copyValue}</i>
+                            : <p className="mt-2">Click to regeneration button for generate  invite link</p>
+                        }
                     </ListGroup.Item>
                     <ListGroup.Item>
-                        <Button className={style.btn}><Icon icon="akar-icons:copy" /> Copy to clipboard</Button>
-                        <Button className={style.btn}>Regeneration</Button>
+                        <CopyToClipboard text={copyValue} onCopy={() => { setCopied(true) }}>
+                            <Button disabled={copyValue ? false : true} className={style.btn}>
+                                <Icon icon="akar-icons:copy" />
+                                {copied ? ` Copied` : "Copy to clipboard"}
+                            </Button>
+                        </CopyToClipboard>
+                        <Button disabled={loadingRege} onClick={() => { handleRegeneration() }} className={style.btn}>
+                            {loadingRege ? <PulseLoader size={10} /> : " Regeneration"}
+                        </Button>
                     </ListGroup.Item>
                 </ListGroup>
             </Card>
@@ -82,9 +125,9 @@ const Setting = () => {
                     <ListGroup.Item className="pb-3">
                         <Form onSubmit={handleSubmit}>
                             <Form.Group className="input-group mb-3 mt-3" controlId="formBasicEmail">
-                                <Form.Control disabled={loading} onChange={(e)=>{setEmail(e.target.value)}} type="email" placeholder="Enter email" />
+                                <Form.Control disabled={loading} onChange={(e) => { setEmail(e.target.value) }} type="email" placeholder="Enter email" />
                                 <Button disabled={loading} variant="primary" type="submit">
-                                {loading ? <PulseLoader size={10} /> : "Send"}
+                                    {loading ? <PulseLoader size={10} /> : "Send"}
                                 </Button>
                             </Form.Group>
 
