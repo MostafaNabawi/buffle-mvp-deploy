ce0eed2c316d61bded80699d4aa8cdf9ed39b988
Abbas roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2022-01-31 14:48:50 +0430

    list event users

--- src/components/moneyPool/partials/PersonSelectorDropDown.jsx
+++ src/components/moneyPool/partials/PersonSelectorDropDown.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Icon } from "@iconify/react";
 import { Dropdown, DropdownButton } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
@@ -6,22 +7,28 @@ import { setSelectedUserID } from "../../../store/moneyPoolSlice";
 function PersonSelectorDropDown(props) {
   const { eventUsers } = useSelector((state) => state.moneyPool);
   const dispatch = useDispatch();
-  const [userID, serUserID] = useState("");
-  //useEffect function
-  useEffect(() => {
-    dispatch(setSelectedUserID(userID));
-  }, [userID]);
+  const title =
+    eventUsers !== "" ? (
+      `${eventUsers[0].first_name} ${eventUsers[0].last_name} `
+    ) : (
+      <Icon fontSize={24} icon="eos-icons:loading" />
+    );
+  const handleSetID = (id) => {
+    dispatch(setSelectedUserID(id));
+  };
+
   return (
     <DropdownButton
       id="dropdown-basic-button"
-      // title={eventUsers[0].name}
+      title={title}
       variant="secondary"
     >
-      {/* {eventUsers.map((item) => (
-        <Dropdown.Item key={item.name} onClick={() => serUserID(item.name)}>
-          {item.name}
-        </Dropdown.Item>
-      ))} */}
+      {eventUsers &&
+        eventUsers.map((item) => (
+          <Dropdown.Item key={item.id} onClick={() => handleSetID(item.id)}>
+            {`${item.first_name} ${item.last_name} `}
+          </Dropdown.Item>
+        ))}
     </DropdownButton>
   );
 }
