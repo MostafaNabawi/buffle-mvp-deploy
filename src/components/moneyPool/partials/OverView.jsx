84a5ec0149661d91e5060dc2238612176256b9b9
Abbas roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2022-02-02 14:44:35 +0430

    finish show overveiw details

--- src/components/moneyPool/partials/OverView.jsx
+++ src/components/moneyPool/partials/OverView.jsx
@@ -1,31 +1,48 @@
 import React from "react";
 import { Table } from "react-bootstrap";
-
-function OverView() {
+import style from "./../style.module.css";
+function OverView(props) {
+  const { data } = props;
   return (
     <div>
       <Table striped>
         <thead>
           <tr>
             <td> This event cost the group</td>
-            <th>$122.00</th>
+            <th>{data.groupCost}</th>
           </tr>
         </thead>
         <tbody>
           <tr>
             <td>It cost you</td>
-            <th>$61.00</th>
+            <th>{data.costYou}</th>
           </tr>
           <tr>
             <td>You've paid</td>
-            <th>$121,334.00</th>
+            <th>{data.paied}</th>
           </tr>
           <tr>
+            <td>You are owed</td>
+            <th>{data.owed}</th>
+          </tr>
+          {/* <tr>
             <td>You've received</td>
             <th>$121,273.00</th>
-          </tr>
+          </tr> */}
         </tbody>
       </Table>
+
+      {data.lent.length > 0 && (
+        <div className={style.settle}>
+          <div className={style.header}>
+            <h4>How to settle all debts</h4>
+          </div>
+          <div className={style.settle_content}>
+            {data &&
+              data.lent.map((item) => <div key={item.msg}>{item.msg}</div>)}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
