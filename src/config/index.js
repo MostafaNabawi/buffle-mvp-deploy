95e7264fc905f0b51d2bb4625cb1ebae817c0348
Paiman-Rasoli <paimanrasoli789@gmail.com>
2022-02-07 10:50:14 +0430

    add sound on notification done

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
-// const API_URL = process.env.API_URL || "http://localhost:8000/api";
+// const API_URL = process.env.API_URL || "http://localhost:8000/api"
 const API_URL =
-process.env.API_URL || "https://buffle-api-test.herokuapp.com/api";
+  process.env.API_URL || "https://buffle-api-test.herokuapp.com/api";
 const GOOGLE_CLIENT_ID =
   process.env.GOOGLE_CLIENT_ID ||
   "932914640198-g4eg34uccat5u6roh3ds7mmkmds2b58j.apps.googleusercontent.com";
