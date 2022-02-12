cb44083bbcede68b2dd510b1084a4b67777799dd
sayedhassan-husssaini <sh.hussaini1378@gmail.com>
2022-02-12 10:34:59 +0430

    create feel page

---
+++ src/components/feel/CartReport.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const  CharReport =()=> {
+    return (
+        <h1>Feel report</h1>
+    )
+}
+export default CharReport;
---
+++ src/pages/feelReport.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import CharReport from "../components/feel/CartReport";
+
+const  FeelReport =()=> {
+    return (
+        <CharReport/>
+    )
+}
+export default FeelReport;
