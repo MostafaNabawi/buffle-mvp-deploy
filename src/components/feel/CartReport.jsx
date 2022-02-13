069ddcd55a1a3a2b8f4bf8b38b411fe1ff028517
sayedhassan-husssaini <sh.hussaini1378@gmail.com>
2022-02-13 14:23:42 +0430

    complated feel chart

--- src/components/feel/CartReport.jsx
+++ src/components/feel/CartReport.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import style from "./style.module.css";
-import moment from "moment";
+import { Icon } from '@iconify/react';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -14,7 +14,6 @@ import {
 import { Line } from "react-chartjs-2";
 import { Card, Row, Col, Form } from "react-bootstrap";
 import { API_URL } from "../../config";
-const { faker } = require("@faker-js/faker");
 
 ChartJS.register(
   CategoryScale,
@@ -37,12 +36,33 @@ export const options = {
       text: "Feel report",
     },
   },
+  interaction: {
+    intersect: false,
+  },
+  scales: {
+    x: {
+      display: true,
+      title: {
+        display: true,
+      },
+    },
+    y: {
+      display: true,
+      title: {
+        display: true,
+        text: "Percent",
+      },
+      suggestedMin: 0,
+      suggestedMax: 100,
+    },
+  },
 };
 
 const labels = ["", "1 Week", "2 Week", "3 Week", "4 Week"];
 
 const CharReport = () => {
   const [basy, setBasy] = useState(false);
+  const [currentMonth, setCurrentMonth] = useState("");
   //
   const [smiling, setSmiling] = useState([]);
   const [happy, setHappy] = useState([]);
@@ -58,30 +78,45 @@ const CharReport = () => {
         data: happy,
         borderColor: "rgb(235,129,115)",
         backgroundColor: "rgba(235,129,115, 0.5)",
+        fill: false,
+        cubicInterpolationMode: "monotone",
+        tension: 0.6,
       },
       {
         label: "Smiling",
         data: smiling,
         borderColor: "rgb(238,180,21)",
         backgroundColor: "rgba(238,180,21, 0.5)",
+        fill: false,
+        cubicInterpolationMode: "monotone",
+        tension: 0.9,
       },
       {
         label: "Normal",
         data: normal,
         borderColor: "rgb(122,252,67)",
         backgroundColor: "rgba(122,252,67, 0.5)",
+        fill: false,
+        cubicInterpolationMode: "monotone",
+        tension: 0.8,
       },
       {
         label: "Sad",
         data: sad,
         borderColor: "rgb(52,109,139)",
         backgroundColor: "rgba(52,109,139, 0.5)",
+        fill: false,
+        cubicInterpolationMode: "monotone",
+        tension: 0.7,
       },
       {
         label: "Crying",
         data: cry,
         borderColor: "rgb(25,55,105)",
         backgroundColor: "rgba(25,55,105, 0.5)",
+        fill: false,
+        cubicInterpolationMode: "monotone",
+        tension: 0.5,
       },
     ],
   };
@@ -98,7 +133,6 @@ const CharReport = () => {
     normal.push(n);
     sad.push(sa);
     cry.push(c);
-    console.log("smi...",smiling)
   };
   const countEachFeel = (data) => {
     var s = 0;
@@ -139,7 +173,6 @@ const CharReport = () => {
         normal.push(0);
         sad.push(0);
         cry.push(0);
-        console.log("payload", payload);
         countEachFeel(payload);
       });
     } catch {
@@ -148,40 +181,69 @@ const CharReport = () => {
   };
   useEffect(() => {
     const d = new Date();
+    setCurrentMonth(d.getMonth() + 1);
     getFeeling(d.getMonth() + 1);
   }, []);
-console.log("s..",smiling)
+  
   return (
     <Row className={`p-0 m-0`}>
       <Card className={`${style.cardReport}`}>
         <Col className="col-2">
           <Form.Select
             onChange={(e) => {
-              smiling.length=0
-              happy.length=0
-              normal.length=0
-              sad.length=0
-              cry.length=0
-              console.log("value...", e.target.value);
-              getFeeling(e.target.value);
+              if (e.target.value != "0") {
+                smiling.length = 0;
+                happy.length = 0;
+                normal.length = 0;
+                sad.length = 0;
+                cry.length = 0;
+                getFeeling(e.target.value);
+              }
             }}
             aria-label="Default select example"
           >
-            <option value="1">January</option>
-            <option value="2">February</option>
-            <option value="4">March</option>
-            <option value="5">April</option>
-            <option value="6">May</option>
-            <option value="7">June</option>
-            <option value="8">July</option>
-            <option value="9">August</option>
-            <option value="10">Sebtember</option>
-            <option value="11">October</option>
-            <option value="12">November</option>
+            <option value="1" selected={currentMonth === 1 ? true : false}>
+              January
+            </option>
+            <option value="2" selected={currentMonth === 2 ? true : false}>
+              February
+            </option>
+            <option value="3" selected={currentMonth === 3 ? true : false}>
+              March
+            </option>
+            <option value="4" selected={currentMonth === 4 ? true : false}>
+              April
+            </option>
+            <option value="5" selected={currentMonth === 5 ? true : false}>
+              May
+            </option>
+            <option value="6" selected={currentMonth === 6 ? true : false}>
+              June
+            </option>
+            <option value="7" selected={currentMonth === 7 ? true : false}>
+              July
+            </option>
+            <option value="8" selected={currentMonth === 8 ? true : false}>
+              August
+            </option>
+            <option value="0" selected={currentMonth === 0 ? true : false}>
+              Sebtember
+            </option>
+            <option value="10" selected={currentMonth === 10 ? true : false}>
+              October
+            </option>
+            <option value="11" selected={currentMonth === 11 ? true : false}>
+              November
+            </option>
+            <option value="12" selected={currentMonth === 12 ? true : false}>
+             December
+            </option>
           </Form.Select>
         </Col>
         {basy ? (
-          "Loading..."
+          <div className="text-center pt-5 mt-5 pb-5 mb-5">
+            <Icon fontSize={44} icon="eos-icons:bubble-loading" />
+          </div>
         ) : (
           <>
             <Line options={options} data={data} />
