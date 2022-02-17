289f25ed310cb21247a8e38171aa395564a190bc
Paiman-Rasoli <paimanrasoli789@gmail.com>
2022-02-17 11:18:15 +0430

    cleaning the project 50%

--- src/pages/taskManage.jsx
+++ src/pages/taskManage.jsx
@@ -1,40 +1,50 @@
 import React, { useState } from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-import '../styles/task.css';
+import "../styles/task.css";
 import TaskManagement from "../components/taskMnagement/TaskManagement";
-import { Card, Row, Col } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import ProjectManagement from "../components/taskMnagement/project/projectManagement";
 const TaskManage = () => {
-    const [value, setValue] = useState('');
-    const [changeColor, setChangeColor] = useState('');
-    const [drop, setDrop] = useState('');
-    const handleChange = (value) => {
-        setValue(value)
-    }
+  const [value, setValue] = useState("");
+  const [changeColor, setChangeColor] = useState("");
+  const [drop, setDrop] = useState("");
+  const handleChange = (value) => {
+    setValue(value);
+  };
 
-    const colorChange = (val) => {
-        setChangeColor(val);
-    }
-    const handleDrop = (value) => {
-        setDrop(value);
-    }
-    return (
-        <Card className="secondary-color taskManage ">
-            {/* <Row className="task-management-filter-row">
+  const colorChange = (val) => {
+    setChangeColor(val);
+  };
+  const handleDrop = (value) => {
+    setDrop(value);
+  };
+  return (
+    <Card className="secondary-color taskManage ">
+      {/* <Row className="task-management-filter-row">
                 <Col>
                     <div className="task-management-filter">
                         <span>Important today items</span>
                     </div>
                 </Col>
             </Row> */}
-            <DndProvider backend={HTML5Backend}>
-                <TaskManagement handleGet={handleChange} val={value} colChange={changeColor} projectDroped={drop} />
-                <ProjectManagement value={value} handleGet={handleChange} colorChange={colorChange} handleDrop={handleDrop} pDrope={drop} />
-            </DndProvider>
-
-        </Card>
-    )
-}
+      <DndProvider backend={HTML5Backend}>
+        <TaskManagement
+          handleGet={handleChange}
+          val={value}
+          colChange={changeColor}
+          projectDroped={drop}
+        />
+        <ProjectManagement
+          value={value}
+          handleGet={handleChange}
+          colorChange={colorChange}
+          handleDrop={handleDrop}
+          pDrope={drop}
+        />
+      </DndProvider>
+    </Card>
+  );
+};
 
 export default TaskManage;
