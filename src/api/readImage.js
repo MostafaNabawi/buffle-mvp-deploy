f6c7a677d3572e6d6c4818f19b661104532dfc56
Paiman-Rasoli <paimanrasoli789@gmail.com>
2022-02-18 20:41:35 +0430

    handle error fetching while networkError

--- src/api/readImage.js
+++ src/api/readImage.js
@@ -8,6 +8,9 @@ async function readImage(key) {
   const req = await fetch(`${API_URL}/user/my-avatar?key=${key}`, {
     credentials: "include",
   });
+  if (req.status === 500) {
+    return "/icone/WB_Headshots-102-web 1.png";
+  }
   const main = await req.blob().then((myBlob) => {
     var objectURL = URL.createObjectURL(myBlob);
     return objectURL;
