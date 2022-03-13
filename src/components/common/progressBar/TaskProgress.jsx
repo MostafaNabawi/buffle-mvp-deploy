f2c6cf2520a2ae15c1af158adb205a65b1e68bdd
ziayamin <ziayamin8@gmail.com>
2022-03-13 11:48:50 +0430

    cahnge color

--- src/components/common/progressBar/TaskProgress.jsx
+++ src/components/common/progressBar/TaskProgress.jsx
@@ -177,13 +177,13 @@ const Timer = (props) => {
                 parseInt(minute) === 0 &&
                 parseInt(hour) === 0 ? (
                 <>
-                  <span className={percent >= 60 ? "" : "showTimeTask2"}>
+                  <span className={percent >= 40 ? "" : "showTimeTask"}>
                     {task_duration}
                   </span>
                 </>
               ) : (
                 <>
-                  <span className={percent >= 60 ? "" : "showTimeTask"}>
+                  <span className={percent >= 40 ? "" : "showTimeTask"}>
                     {`${parseInt(hour) < 10
                       ? "0" + parseInt(hour)
                       : parseInt(hour)
