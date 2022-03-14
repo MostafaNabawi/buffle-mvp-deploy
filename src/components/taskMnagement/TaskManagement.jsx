c69c027532ad733914a45dbeb35a134d74c84fb3
ziayamin <ziayamin8@gmail.com>
2022-03-14 10:16:48 +0430

    chage date

--- src/components/taskMnagement/TaskManagement.jsx
+++ src/components/taskMnagement/TaskManagement.jsx
@@ -175,15 +175,15 @@ const TaskManagement = ({ handleGet, val, colChange, projectDroped }) => {
       }
     });
   };
-  let curr = new Date
-  let week = []
+  let curr = new Date();
+  let week = [];
 
   for (let i = 1; i <= 7; i++) {
-    let first = curr.getDate() - curr.getDay() + i
-    let day = new Date(curr.setDate(first)).toISOString().slice(0, 10)
-    week.push(day)
+    let first = curr.getDate() - curr.getDay() + i;
+    let day = new Date(curr.setDate(first)).toISOString().slice(0, 10);
+    week.push(day);
 
-  }
+  };
   return (
     <Row>
       {statuses.map((s) => {
